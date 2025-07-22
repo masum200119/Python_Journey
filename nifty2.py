63f05dec10cf1a9125315634c15b758e32e6fa5e
Masum Billah <95751615+masum200119@users.noreply.github.com>
2025-07-23 00:03:32 +0600

    Create nifty2.py

---
+++ nifty2.py
@@ -0,0 +1,20 @@
+pairs = []
+with open('input.txt') as file:
+    lines = [line.strip() for line in file if line.strip()]
+i=0
+while i < len(lines):
+    line= lines[i]
+    i=i+1
+    if i<len(lines) and lines[i].replace(' ','').isdigit():
+        numbers=list(map(int,lines[i].split()))
+        i=i+1
+        
+        top3=sorted(numbers,reverse=True)[:len(numbers)]
+        avg=sum(top3)/3
+    else:
+        avg=0
+    pairs.append((line, avg))
+n1=int(input())
+
+for pair in pairs[:n1]:
+    print(f"{pair[0]}: {pair[1]:.2f}")
